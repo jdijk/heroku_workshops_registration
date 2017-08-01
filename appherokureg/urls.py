8ffa0249a5e3a09ad584890220fa1e373ab39489
Felipe Ryan <fryan@fryan-ltm22.internal.salesforce.com>
2017-08-01 16:21:14 +1000

    fixed little url bug

--- appherokureg/urls.py
+++ appherokureg/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^events/(?P<slug>[a-zA-Z0-9_]+)/registration-added/$', views.registration_added, name='registration_added'),
     url(r'^events/(?P<slug>[a-zA-Z0-9_]+)$', views.add_registration, name='add_registration'),
-    url(r'^events/(?P<key>[a-z0-9]{30})$', views.invitee_attended, name='attended'),
+    url(r'^attended/(?P<key>[a-z0-9]{30})$', views.invitee_attended, name='attended'),
 ]
